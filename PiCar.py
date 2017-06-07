b405dd889f6781dd97f0be0e2637a9d721da242e
Tyler Wright <tw39440@gmail.com>
2017-06-07 13:01:23 -0400

    motor functions and display
    cannot test code at this time, will need to fix on Pi this weekend

--- PiCar.py
+++ PiCar.py
@@ -4,6 +4,8 @@ from matplotlib import pyplot as plt
 import PiCamera as cam
 from time import sleep
 import RPi.GPIO as GPIO
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+import atexit
 
 # accesses stored images for cross-checking with the feed
 # TODO: store processed versions of these images
@@ -18,16 +20,66 @@ camera.start_preview()
 camera.capture('capture.png')
 cap = cv2.imgread('capture.png', 0)
 
+# create a default object, no changes to I2C address or frequency
+mh = Adafruit_MotorHAT(addr=0x60)
+
+# defines motors
+left = mh.getMotor(1)
+right = mh.getMotor(3)
+
 # processes image for cross comparison with capture
 # returns black and white image after Gaussian blur
 def process(img):
     img = cv2.medianBlur(img,5)
+
+    ret,th1 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
+
+    return img
+
+def plot(img):
+    plt.subplot(2, 2, 2)
+    plt,imshow(img, 'gray')
+    plt.title('processed image')
+    plt.xticks([])
+    plt.yticks([])
+    plt.show
     
-
+# motor functions
 def forward():
-
+    left.run(Adafruit_MotorHAT.FORWARD)
+    right.run(Adafruit_MotorHAT.FORWARD)
+    
+    left.setSpeed(100)
+    right.setSpeed(100)
 
 def left():
+    left.run(Adafruit_MotorHAT.BACKWARD)
+    right.run(Adafruit_MotorHAT.FORWARD)
 
-
+    left.setSpeed(100)
+    right.setSpeed(100)
+    
 def right():
+    left.run(Adafruit_MotorHAT.FORWARD)
+    right.run(Adafruit_MotorHAT.BACKWARD)
+
+    left.setSpeed(100)
+    right.setSpeed(100)
+
+def stop():
+    left.setSpeed(0)
+    right.setSpeed(0)
+
+    left.run(Adafruit_MotorHAT.RELEASE)
+    right.run(Adafruit_MotorHAT.RELEASE)
+    
+
+# recommended for auto-disabling motors on shutdown!
+def turnOffMotors():
+	mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+atexit.register(turnOffMotors)
